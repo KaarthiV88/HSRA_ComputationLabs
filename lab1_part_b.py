9637f010ba844f681e1717705f71436215512e6e
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2023-06-21 09:21:46 -0500

    Add 'moveCalc()' lab1_part_b.py

--- lab1_part_b.py
+++ lab1_part_b.py
@@ -13,3 +13,12 @@ def accelerationCalc(force, mass):
     return "Acceleration: " + str(float(force/mass))
 
 print(accelerationCalc(10, 7))
+
+def moveCalc(velocity, acceleration):
+    delta_t = 0.1
+    delta_r = "{:.2f}".format(velocity*delta_t + 0.5*acceleration*(delta_t**2))
+    #delta_r = "{:.2f}".format(delta_r)
+    new_velocity = "{:.2f}".format(acceleration * delta_t)
+    return "Movement: " + str(delta_r) + "m\n" + "New Velocity: " + str(new_velocity) + "m\s"
+
+print(moveCalc(10, 3))
