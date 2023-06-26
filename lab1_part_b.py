ded546f70eaa125186be8d6d80e19be8482c4eb9
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2023-06-25 19:38:03 -0500

    Update lab1_part_b.py

--- lab1_part_b.py
+++ lab1_part_b.py
@@ -1,38 +1,78 @@
 import math
 import numpy as np
 
-#Damped Molecular Dynamics
+#Part A: Damped Molecular Dynamics
 
 def forceReturn(pos):
-    Vlj = 4*1*(-12*(1/pos)**13 + 6 * (1/pos)**7)
-    return Vlj
+    Flj = 4*1*(-12*(1/pos)**13 + 6 * (1/pos)**7)
+    return Flj
 
 print(forceReturn(0.3))
 
-def accelerationCalc(force, mass):
-    return "Acceleration: " + str(float(force/mass))
+def accelerationCalc(force):
+    acceleration = force/1
+    return acceleration
 
-print(accelerationCalc(10, 7))
+print(accelerationCalc(10))
 
-def moveCalc(velocity, acceleration):
-    delta_t = 0.1
-    delta_r = "{:.2f}".format(velocity*delta_t + 0.5*acceleration*(delta_t**2))
+
+def moveCalc(velocity, acc):
+    delta_t = 0.01
+    velocity = (acc * delta_t)
+    delta_r = velocity * delta_t + 0.5 * float(acc) * delta_t ** 2
     #delta_r = "{:.2f}".format(delta_r)
-    new_velocity = "{:.2f}".format(acceleration * delta_t)
-    return "Movement: " + str(delta_r) + "m\n" + "New Velocity: " + str(new_velocity) + "m\s"
+    
+    return delta_r, velocity
 
 print(moveCalc(10, 3))
 
-def damper(posChange, velocity):
+    
+
+def damper(delta_r, velocity):
     threshold = 0.1
-    if(posChange > threshold):
-        posChange = threshold
-        velocity = 0    
+    if(delta_r > threshold):
+        threshold = delta_r
+        velocity = 0
+    
     else:
         velocity = 0.9*velocity
         
-def updater(currentPos, posChange):
-    newPosition = currentPos + posChange
-    return newPosition
+    return delta_r, velocity
+        
+def updater_r(r, delta_r):
+    r = r + delta_r
+    return r
 
-print(updater(10, 5))
+def get_force(r):
+    force = r
+    return force
+
+print("__________________________________________")
+#___________________________________________________________________________________________________________________________________________________
+r = 0.3
+velocity = 0
+delta_r = 0
+acceleration = 0
+force = 100
+get_delta_r_v = 0
+damp_delta_r_v = 0  
+
+while(abs(force) > 0.01):
+    force = get_force(force)
+    #print(force)
+    acceleration = accelerationCalc(force)
+    #print(acceleration)
+    x = moveCalc(velocity, acceleration)
+    velocity = x[1]
+    delta_r = x[0]
+    #print(velocity)
+    #print(delta_r)
+    #print(damper(delta_r, velocity))
+    r = updater_r(r, delta_r)
+    #print(r)
+    #print(force)
+    force = forceReturn(r)
+    #print(force)
+    
+    print("r = " + str(r) + "\nForce = " + str(force))
+
